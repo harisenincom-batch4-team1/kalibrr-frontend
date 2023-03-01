7327197305ead9d938612324111a6300978cc28c
Mukhtar Rafi Fauzi <mukhtar.r.f@gmail.com>
2023-03-01 22:25:11 +0700

    add assets and fixing mobile view

--- src/components/homepage/ExploreJob.js
+++ src/components/homepage/ExploreJob.js
@@ -4,15 +4,17 @@ const ExploreJobs = () => {
   return (
     <div className="mb-10">
       <div className="container mx-auto mt-10 p-5 bg-[#f7f9fb] rounded-lg h-fit flex flex-col justify-center">
-        <div className="container mx-auto">
-          <h1 className="text-2xl text-center md:text-left md:text-4xl  font-bold">
+        <div className="container mx-auto px-10 py-5">
+          <h1 className="text-2xl text-center md:text-4xl  font-bold">
             Jelajahi pekerjaan populer.
           </h1>
-          <p className="mt-4 text-center md:text-left md:text-xl">
+          <p className="mt-4 text-center md:text-xl">
             Lihat pekerjaan unggulan dari perusahaan populer
           </p>
-          <p className="text-center md:text-left md:text-xl ">Mulai melamar sekarang, atau</p>
-          <div className="mt-4 flex w-full justify-center md:justify-start">
+          <p className="text-center md:text-xl ">
+            Mulai melamar sekarang, atau
+          </p>
+          <div className="mt-4 flex w-full justify-center">
             <a
               href="/"
               className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -35,31 +37,30 @@ const ExploreJobs = () => {
           </div>
         </div>
 
-        <div className="w-full grid xl:grid-cols-3 xl:grid-rows-2 md:grid-cols-2 px-5 py-5 mx-auto mt-10 justify-center gap-5">
+        <div className="w-full grid xl:grid-cols-3 xl:grid-rows-2 md:grid-cols-2 px-5 py-5 mt-10 gap-5">
           <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 space-x-5">
             <a href="/">
               <div className="container flex flex-row grow-0 h-26 bg-white">
                 <div className="flex items-center">
                   <img
-                    src={"/assets/companylogo.png"}
+                    src={"/assets/BCALogo.png"}
                     alt=""
-                    className="w-16 items-center mx-auto"
+                    className="w-28 items-center mx-auto"
                   />
                 </div>
                 <div className="container grow-0 pb-4">
-                  <p className="text-lg font-bold mt-4 pl-4">A Company </p>
+                  <p className="text-lg font-bold mt-4 pl-4">Bank BCA </p>
                 </div>
               </div>
             </a>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Fullstack Web Developer
+              Staf Rekrutmen
             </h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Responsibilities: Developing front-end web architecture
-              Developing/extending front-end components. Ensuring responsiveness
-              and scalability of applications. Understand and translate business
-              requirements into a functional and appealing interactive
-              application. Collaborate with backend engineer...
+              Responsibilities: Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Blanditiis corporis nihil reiciendis animi
+              dolorem inventore quod obcaecati modi cupiditate, aliquam autem
+              officia error veniam consectetur quam nobis maiores eius. Omnis!
             </p>
             <a
               href="/"
@@ -74,25 +75,24 @@ const ExploreJobs = () => {
               <div className="container flex flex-row grow-0 h-26 bg-white">
                 <div className="flex items-center">
                   <img
-                    src={"/assets/companylogo.png"}
+                    src={"/assets/RuangguruLogo.png"}
                     alt=""
-                    className="w-16 items-center mx-auto"
+                    className="w-28 items-center mx-auto"
                   />
                 </div>
                 <div className="container grow-0 pb-4">
-                  <p className="text-lg font-bold mt-4 pl-4">A Company </p>
+                  <p className="text-lg font-bold mt-4 pl-4">Ruangguru</p>
                 </div>
               </div>
             </a>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Fullstack Web Developer
+              Freelance Tutor
             </h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Responsibilities: Developing front-end web architecture
-              Developing/extending front-end components. Ensuring responsiveness
-              and scalability of applications. Understand and translate business
-              requirements into a functional and appealing interactive
-              application. Collaborate with backend engineer...
+              Responsibilities: Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Blanditiis corporis nihil reiciendis animi
+              dolorem inventore quod obcaecati modi cupiditate, aliquam autem
+              officia error veniam consectetur quam nobis maiores eius. Omnis!
             </p>
             <a
               href="/"
@@ -107,25 +107,24 @@ const ExploreJobs = () => {
               <div className="container flex flex-row grow-0 h-26 bg-white">
                 <div className="flex items-center">
                   <img
-                    src={"/assets/companylogo.png"}
+                    src={"/assets/Tokopedialogo.png"}
                     alt=""
-                    className="w-16 items-center mx-auto"
+                    className="w-28 items-center mx-auto"
                   />
                 </div>
                 <div className="container grow-0 pb-4">
-                  <p className="text-lg font-bold mt-4 pl-4">A Company </p>
+                  <p className="text-lg font-bold mt-4 pl-4">Tokopedia</p>
                 </div>
               </div>
             </a>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Fullstack Web Developer
+              Backend Engineer
             </h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Responsibilities: Developing front-end web architecture
-              Developing/extending front-end components. Ensuring responsiveness
-              and scalability of applications. Understand and translate business
-              requirements into a functional and appealing interactive
-              application. Collaborate with backend engineer...
+              Responsibilities: Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Blanditiis corporis nihil reiciendis animi
+              dolorem inventore quod obcaecati modi cupiditate, aliquam autem
+              officia error veniam consectetur quam nobis maiores eius. Omnis!
             </p>
             <a
               href="/"
@@ -140,25 +139,24 @@ const ExploreJobs = () => {
               <div className="container flex flex-row grow-0 h-26 bg-white">
                 <div className="flex items-center">
                   <img
-                    src={"/assets/companylogo.png"}
+                    src={"/assets/BNIlogo.png"}
                     alt=""
-                    className="w-16 items-center mx-auto"
+                    className="w-28 items-center mx-auto"
                   />
                 </div>
                 <div className="container grow-0 pb-4">
-                  <p className="text-lg font-bold mt-4 pl-4">A Company </p>
+                  <p className="text-lg font-bold mt-4 pl-4">Bank BNI</p>
                 </div>
               </div>
             </a>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Fullstack Web Developer
+              Security Engineer
             </h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Responsibilities: Developing front-end web architecture
-              Developing/extending front-end components. Ensuring responsiveness
-              and scalability of applications. Understand and translate business
-              requirements into a functional and appealing interactive
-              application. Collaborate with backend engineer...
+              Responsibilities: Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Blanditiis corporis nihil reiciendis animi
+              dolorem inventore quod obcaecati modi cupiditate, aliquam autem
+              officia error veniam consectetur quam nobis maiores eius. Omnis!
             </p>
             <a
               href="/"
@@ -173,25 +171,24 @@ const ExploreJobs = () => {
               <div className="container flex flex-row grow-0 h-26 bg-white">
                 <div className="flex items-center">
                   <img
-                    src={"/assets/companylogo.png"}
+                    src={"/assets/Travelokalogo.webp"}
                     alt=""
-                    className="w-16 items-center mx-auto"
+                    className="w-28 items-center mx-auto"
                   />
                 </div>
                 <div className="container grow-0 pb-4">
-                  <p className="text-lg font-bold mt-4 pl-4">A Company </p>
+                  <p className="text-lg font-bold mt-4 pl-4">Traveloka</p>
                 </div>
               </div>
             </a>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Fullstack Web Developer
+              Site Reliability Engineer
             </h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Responsibilities: Developing front-end web architecture
-              Developing/extending front-end components. Ensuring responsiveness
-              and scalability of applications. Understand and translate business
-              requirements into a functional and appealing interactive
-              application. Collaborate with backend engineer...
+              Responsibilities: Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Blanditiis corporis nihil reiciendis animi
+              dolorem inventore quod obcaecati modi cupiditate, aliquam autem
+              officia error veniam consectetur quam nobis maiores eius. Omnis!
             </p>
             <a
               href="/"
@@ -206,25 +203,24 @@ const ExploreJobs = () => {
               <div className="container flex flex-row grow-0 h-26 bg-white">
                 <div className="flex items-center">
                   <img
-                    src={"/assets/companylogo.png"}
+                    src={"/assets/Shopeelogo.png"}
                     alt=""
-                    className="w-16 items-center mx-auto"
+                    className="w-28 items-center mx-auto"
                   />
                 </div>
                 <div className="container grow-0 pb-4">
-                  <p className="text-lg font-bold mt-4 pl-4">A Company </p>
+                  <p className="text-lg font-bold mt-4 pl-4">Shopee</p>
                 </div>
               </div>
             </a>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Fullstack Web Developer
+              Customer Service (Contract)
             </h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Responsibilities: Developing front-end web architecture
-              Developing/extending front-end components. Ensuring responsiveness
-              and scalability of applications. Understand and translate business
-              requirements into a functional and appealing interactive
-              application. Collaborate with backend engineer...
+              Responsibilities: Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Blanditiis corporis nihil reiciendis animi
+              dolorem inventore quod obcaecati modi cupiditate, aliquam autem
+              officia error veniam consectetur quam nobis maiores eius. Omnis!
             </p>
             <a
               href="/"
